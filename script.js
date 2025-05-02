fee1d6d2831b79482977c1b5a4ca99607315476f
tasnim-rahman <tasnimrahman0101@gmail.com>
2025-05-02 17:41:25 +0600

    border fixed

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
-window.addEventListener('scroll', function() {
+window.addEventListener('scroll', function () {
     const navbar = document.querySelector('.navbar');
+    const menuScroll = this.document.querySelector('.menu-section');
+
     if (window.scrollY > 50) {
         navbar.classList.add('shrink');
+        menuScroll.classList.add('menu-section-scroll');
     } else {
         navbar.classList.remove('shrink');
+        menuScroll.classList.remove('menu-section-scroll');
     }
 });
 
-window.addEventListener('scroll', function() {
+window.addEventListener('scroll', function () {
     const svgs = document.getElementsByClassName('svg');
     for (let i = 0; i < svgs.length; i++) {
         if (window.scrollY > 50) {
